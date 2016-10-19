fd00be92dfe9c6888871043a7ecc1e232d31f39b
Allen Hai <allen.hai@hotmail.com>
2016-10-19 08:39:39 -0500

    white space background, black stars, clean monospace footer

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root "pages#home"
 
   get "squad" => "pages#squad"
+  get "space" => "pages#space"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
